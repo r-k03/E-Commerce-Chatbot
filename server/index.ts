704b4a1518c08dac3e9dfc5be47c5de7c7934507
r-k03 <rahul.kamath2512@gmail.com>
2025-08-21 19:07:22 -0700

    Finished Backend Skeleton

--- server/index.ts
+++ server/index.ts
@@ -8,3 +8,47 @@ app.use(cors());
 app.use(express.json());
 
 const client = new MongoClient(process.env.MONGO_URI as string);
+
+const PORT = process.env.PORT || 5000
+
+async function startServer() {
+    try {
+        await client.connect();
+        await client.db("admin").command({ping: 1});
+        console.log("DB Conn Successful");
+        app.listen(PORT, () => {
+            console.log(`Server Listening on Port ${PORT}`);
+        })
+    } catch (error) {
+        throw new Error("Error Connecting to the Database/Starting Server");
+    }
+}
+
+app.get("/",(req: Request, res: Response) => {
+    res.send("LangGraph Server");
+});
+
+// Start New Conversation
+app.post("/chat", (req: Request, res: Response) => {
+    const {message} = req.body;
+    const id = Date.now().toString();
+    try {
+        // get response
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({"message": "Internal Server Error"});
+    }
+});
+
+// Continue Existing Conversation
+app.post("/chat/:id", (req: Request, res: Response) => {
+    const {id} = req.params;
+    const {message} = req.body;
+    try {
+        // Get Response
+    } catch (error) {
+        console.error("Chat Error", error);
+        res.status(500).json({"message": "Internal Server Error"});
+    }
+
+})
